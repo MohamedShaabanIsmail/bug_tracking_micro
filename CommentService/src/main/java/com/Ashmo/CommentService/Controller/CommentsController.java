f9e558d5911ac6d08b19f84a65119e633cc9e87b
MohamedShaabanIsmail <mohamed_shaaban_ism@outlook.com>
2025-05-14 04:09:11 +0300

    Cros Origin

--- CommentService/src/main/java/com/Ashmo/CommentService/Controller/CommentsController.java
+++ CommentService/src/main/java/com/Ashmo/CommentService/Controller/CommentsController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Ashmo.CommentService.Model.Comments;
 import com.Ashmo.CommentService.Service.CommentsService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/comments")
 public class CommentsController {
--- UserService/src/main/java/com/Ashmo/UserService/Configure/SecurityConf.java
+++ UserService/src/main/java/com/Ashmo/UserService/Configure/SecurityConf.java
@@ -1,8 +1,5 @@
 package com.Ashmo.UserService.Configure;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.Ashmo.UserService.Filter.JwtFilter;
 import com.Ashmo.UserService.Service.MyUserDetailsService;
@@ -41,7 +35,6 @@ public class SecurityConf {
         authorizeHttpRequests(request->request.requestMatchers("/login","/refreshtoken","/register").permitAll()
         .anyRequest().authenticated()).
         httpBasic(Customizer.withDefaults())
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
@@ -60,16 +53,4 @@ public class SecurityConf {
         return config.getAuthenticationManager();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
